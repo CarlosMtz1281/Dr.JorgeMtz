31bdcd4134d3f7b40669a8c24687ea433f79e2b3
Carlos Martinez <carlosh.mtz@hotmail.com>
2024-11-29 15:06:27 -0600

    fixed mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -75,15 +75,7 @@ function App() {
 
   return (
     <div className="App">
-       <Routes>
-          <Route path= "/" element={<Inicio/>}/>
-          <Route path= "/Informacion" element={<Informacion/>}/>
-          <Route path= "/Blog" element={<BlogsPage/>}/>
-          <Route path= "/blogs/MuelasDelJuicio" element={<Blog1/>}/>
-          <Route path= "/blogs/ImplantesDentales" element={<Blog2/>}/>
-          <Route path= "/blogs/ExtraccionDental" element={<Blog3/>}/>
-          <Route path= "/blogs/InjertoHueso" element={<Blog4/>}/>
-        </Routes>
+       {getWindowDimension()}
     </div>
   )
 }
